cca26edef781645975917ffcc4e662cb32636991
Spenser Jones <hello@spenserj.com>
2015-03-02 21:02:01 -0700

    Adding basic gulpfile for React

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-ruby-sass');
+var concat = require('gulp-concat');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify')
+  , sourcemaps = require('gulp-sourcemaps');
+
+function babelJSFiles(path, files, filename) {
+  files = files.map(function (file) { return path + '/src/' + file; });
+  return gulp.src(files)
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat(filename))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(path + '/build/'));
+}
+
+gulp.task('default', function () {
+  gulp.watch('./sites/all/modules/react_example/js/src/*.jsx', ['babel']);
+});
+
+gulp.task('babel', function () {
+  return babelJSFiles('./sites/all/modules/react_example/js', ['*.jsx', 'main.jsx'], 'react_example.js');
+});
