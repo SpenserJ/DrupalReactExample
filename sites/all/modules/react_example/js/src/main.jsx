da22b73b9adbe325ea30eafdd0bf201de36293fa
Spenser Jones <hello@spenserj.com>
2015-03-02 21:18:04 -0700

    Adding a basic example of rendering data out

--- sites/all/modules/react_example/js/src/main.jsx
+++ sites/all/modules/react_example/js/src/main.jsx
@@ -1,8 +1,31 @@
 var Menu = React.createClass({
+  getInitialState: function () {
+    return {
+      // An object full of item ids and their nutritional information.
+      items: {
+        '1': {
+          name: 'Steak',
+          calories: 420,
+        }
+      },
+      // An array of items that make up the user's meal.
+      meal: [],
+    };
+  },
+
   render: function () {
+    var ids = Object.keys(this.state.items);
+    var items = jQuery.map(ids, function (id) {
+      var item = this.state.items[id]
+        , key = 'menu-item-' + id;
+
+      return (<h2 key={key}>{item.name}</h2>);
+    }.bind(this));
+
     return (
       <div className="menu">
         <h1>It works!</h1>
+        {items}
       </div>
     );
   },
