8a7edd1b5fb5bb56be07ca242ad77bc463f38d06
Spenser Jones <hello@spenserj.com>
2015-03-04 19:23:04 -0700

    Adding in a basic filter to the menu list

--- sites/all/modules/react_example/js/src/MenuList.jsx
+++ sites/all/modules/react_example/js/src/MenuList.jsx
@@ -1,21 +1,36 @@
 var MenuList = React.createClass({
   mixins: [Reflux.connect(MenuStore.Store, 'store')],
 
-  render: function () {
-    var ids = Object.keys(this.state.store.items);
-    ids = ids.filter(function (id) {
-      return jQuery.inArray(id, this.state.store.meal) === -1;
-    }.bind(this));
-    var items = jQuery.map(ids, function (id) {
-      var item = this.state.store.items[id]
-        , key = 'menu-item-' + id;
+  getInitialState: function () {
+    return { filter: '', };
+  },
 
-      return (<MenuItem key={key} item={item}/>);
-    }.bind(this));
+  updateFilter: function (e) {
+    this.setState({ filter: e.target.value, });
+  },
+
+  render: function () {
+    var items = Object.keys(this.state.store.items)
+    .filter(function (id) {
+      return jQuery.inArray(id, this.state.store.meal) === -1;
+    }.bind(this))
+    .map(function (id) {
+      return this.state.store.items[id];
+    }.bind(this))
+    .filter(function (item) {
+      return (this.state.filter === ''
+           || item.name.indexOf(this.state.filter) > -1);
+    }.bind(this))
+    .map(function (item) {
+      return (<MenuItem key={'menu-item-' + item.id} item={item} />);
+    });
 
     return (
       <div className="menu-items">
         <h1>Menu Items</h1>
+        <div>
+          <label>Filter: </label><input type="text" size="60" onChange={this.updateFilter} />
+        </div>
         <table>
           <thead>
             <tr>
